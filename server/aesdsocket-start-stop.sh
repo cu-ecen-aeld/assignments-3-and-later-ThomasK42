75135295b205d17d3db468f865e46fb062d96d14
ThomasK42 <tk@softcomponents.de>
2026-03-06 13:06:48 +0100

    Added start-stop script for assignment 5 part 2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Daemon start/stop script for assignment 5
+# Author: Thomas Kloeker
+
+case "$1" in
+	start)
+		echo "Starting aesdsocket"
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+		;;
+	stop)
+		echo "Stopping aesdsocket"
+		start-stop-daemon -K -n aesdsocket 
+		;;
+	*)
+		echo "Usage: $0 {start|stop}"
+		exit 1
+esac
+
+exit 0
